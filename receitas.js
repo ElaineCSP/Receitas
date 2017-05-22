1dc072900c1b929da5e615fb2f1b16096c6a7c83
Elaine Paiva <Elaine Paiva>
2017-05-21 22:40:57 -0300

    organizando css, criando funcao js, limpando html

--- receitas.js
+++ receitas.js
@@ -1,28 +1,34 @@
 
+var divReceitas = document.querySelectorAll('.receita');
+
 var divReceitaBemCasado = document.querySelector('.receita-bemcasado'); 
-//usei '.' pq receita-bemcasado é uma classe
-
 var divReceitaPudim = document.querySelector('.receita-pudim'); 
-//diferente da receita de bem casado, no pudim vc nao colocou uma div para envolve-lo, faça isso para
-// o queryselector funcionar!
-
-divReceitaBemCasado.style.display = 'none';
+var divReceitaTorta = document.querySelector('.receita-torta');
 
 var botaoBemCasado = document.querySelector('#bem-casado');
-//usei '.' pq bem-casado é um id
-
-// é assim que se coloca um evento de click
-botaoBemCasado.addEventListener('click', function() {
-    divReceitaBemCasado.style.display = 'block'; // aqui a div de bem casado aparece
-    divReceitaPudim.style.display = 'none';
-    // aqui a div de pudim some (quer dizer, quando ela tiver uma div envolvendo-a e com uma classe bacanuda pro query selector funcionar)
-})
-
-divReceitaPudim.style.display = 'none';
-
 var botaoPudim = document.querySelector('#pudim');
+var botaoTorta = document.querySelector('#torta');
 
+apagarTodasReceitas();
+divReceitaPudim.style.display = 'block';
+
+
+botaoBemCasado.addEventListener('click', function() {
+    apagarTodasReceitas();
+    divReceitaBemCasado.style.display = 'block'; 
+});
 botaoPudim.addEventListener('click', function() {
+    apagarTodasReceitas();
     divReceitaPudim.style.display = 'block';
-    divReceitaBemCasado.style.display = 'none';
-})
+});
+botaoTorta.addEventListener('click', function() {
+    apagarTodasReceitas();
+    divReceitaTorta.style.display = 'block';
+});
+
+function apagarTodasReceitas() {
+    for (i = 0; i < divReceitas.length; i++) {
+        divReceitas[i].style.display = 'none';
+    }
+}
+
